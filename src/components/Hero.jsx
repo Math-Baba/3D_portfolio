bbdf3bbc55ca477306277bad26a4bb0a61dedf0c
Math-Baba <mathieu.rl.baba@gmail.com>
2025-07-06 21:37:35 +0400

    Modification du titre : responsivité

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
+          <h1 className={`${styles.heroHeadText} text-white text-3xl sm:text-5xl`}>
             Salut, je m'appelle <span className="text-[#915EFF]">Mathieu &nbsp;</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
