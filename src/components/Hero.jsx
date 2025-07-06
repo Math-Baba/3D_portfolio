8c30611f61d4ae3365e30a0a3bda105fb5fda6b0
Math-Baba <mathieu.rl.baba@gmail.com>
2025-07-06 21:55:44 +0400

    Test 3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
 
         <div>
           <h1
-            className={`${styles.heroHeadText} text-white text-3xl sm:text-5xl`}
+            className={`${styles.heroHeadText} text-white text-2xl sm:text-5xl`}
           >
             Salut, je m'appelle{" "}
             <span className="text-[#915EFF]">Mathieu &nbsp;</span>
