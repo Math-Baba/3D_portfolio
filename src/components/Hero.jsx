69f098a203d5981d499ecfdc8ec5fe9f725aab30
Math-Baba <mathieu.rl.baba@gmail.com>
2025-07-06 21:50:01 +0400

    Test 2 : responsivité

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 transform origin-top-left lg:scale-[0.6] lg:ml-16`}
+        className={`px-4 sm:px-8 absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 transform origin-top-left lg:scale-[0.6] lg:ml-16`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
@@ -13,11 +13,15 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white text-2xl sm:text-5xl`}>
-            Salut, je m'appelle <span className="text-[#915EFF]">Mathieu &nbsp;</span>
+          <h1
+            className={`${styles.heroHeadText} text-white text-3xl sm:text-5xl`}
+          >
+            Salut, je m'appelle{" "}
+            <span className="text-[#915EFF]">Mathieu &nbsp;</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Etudiant autodidacte,<br className="sm:block hidden" />
+            Etudiant autodidacte,
+            <br className="sm:block hidden" />
             j'apprends en codant <br className="sm:block hidden" />
             des projets
           </p>
